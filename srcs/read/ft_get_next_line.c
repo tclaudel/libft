36849396a93b431a0f382e0e4ed7774a72864d2b
Thomas Claudel <tclaudel@z2r1p3.le-101.fr>
2019-12-06 16:57:18 +0100

    GNL \n suppressed

--- srcs/read/ft_get_next_line.c
+++ srcs/read/ft_get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tclaudel <tclaudel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/28 13:33:48 by tclaudel     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/03 14:22:49 by tclaudel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/06 16:56:20 by tclaudel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -41,7 +41,7 @@ size_t		overflow(char **line, char **str, size_t *size, char *nl_pointer)
 	if ((*line = (char *)malloc(line_size + 1)) == NULL)
 		return (error(str, size));
 	ft_memcpy(*line, *str, line_size);
-	(*line)[line_size] = '\0';
+	(*line)[line_size - 1] = '\0';
 	*size -= line_size;
 	ft_memcpy(*str, *str + line_size, *size);
 	return ((ssize_t)line_size);
