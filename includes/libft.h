5f9cba98d14a1d99bccb16bf84eacf9ef26a20e6
Thomas Claudel <contact@tclaudel.fr>
2020-01-29 10:02:27 +0100

    add color to libft

--- includes/libft.h
+++ includes/libft.h
@@ -6,23 +6,35 @@
 /*   By: tclaudel <tclaudel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/07 13:46:51 by tclaudel     #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/28 16:08:06 by tclaudel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/29 10:01:46 by tclaudel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
+# define BLUE "\033[0;38;5;123m"
+# define LIGHT_PINK "\033[0;38;5;200m"
+# define PINK "\033[0;38;5;198m"
+# define DARK_BLUE "\033[1;38;5;110m"
+# define GREEN "\033[1;32;111m"
+# define LIGHT_GREEN "\033[0;38;5;121m"
+# define LIGHT_RED "\033[0;31;5;110m"
+# define FLASH_GREEN "\033[33;32m"
+# define WHITE_BOLD "\033[37m"
+# define GREY "\033[3;90m"
+# define ORANGE "\033[3;91m"
+# define YELLOW "\033[0;33m"
+# define RESET   "\033[0m"
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
 
-typedef		struct s_strhash
+typedef struct	s_strhash
 {
 	char	*key;
 	char	*value;
-}					t_strhash;
+}				t_strhash;
 
 /*
 ** ALLOC
