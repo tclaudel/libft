6441be3b6591c9e63fbc9e91ad59259ddc61750f
Thomas Claudel <contact@tclaudel.fr>
2020-02-18 15:05:07 +0100

    ft_insert.c

--- srcs/str/ft_insert.c
+++ srcs/str/ft_insert.c
@@ -6,20 +6,19 @@
 /*   By: tclaudel <tclaudel@student.le-101.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 11:43:40 by tclaudel          #+#    #+#             */
-/*   Updated: 2020/02/17 13:06:57 by tclaudel         ###   ########lyon.fr   */
+/*   Updated: 2020/02/17 15:13:41 by tclaudel         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_insert(char *src, char *insered, size_t n, int free_option)
+char	*ft_insert(char *src, char *insered, size_t n, int len)
 {
 	char	*dest;
 
 	dest = ft_strndup(src, n);
 	dest = ft_strfjoin(dest, insered, 1);
-	dest = ft_strfjoin(dest, src + n, 1);
-	if (free_option == 1)
-		free(src);
+	dest = ft_strfjoin(dest, src + n + len, 1);
+	free(src);
 	return (dest);
 }
